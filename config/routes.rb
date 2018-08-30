3dbb599b3adb2185b111d41aa1cb17a06a4749bc
GabrielValle91 <gvalle91@gmail.com>
2018-08-30 15:20:41 -0500

    added api resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  resources :lost_pet_pictures
-  resources :found_pet_pictures
-  resources :found_pet_comments
-  resources :found_pets
-  resources :lost_pet_comments
-  resources :lost_pets
-  resources :users
+  namespace :api do
+    resources :lost_pet_pictures, except: [:new, :edit]
+    resources :found_pet_pictures, except: [:new, :edit]
+    resources :found_pet_comments, except: [:new, :edit]
+    resources :found_pets, except: [:new, :edit]
+    resources :lost_pet_comments, except: [:new, :edit]
+    resources :lost_pets, except: [:new, :edit]
+    resources :users
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
