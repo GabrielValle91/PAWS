24e14f8dfcd34b84c6182b6381622d2bd2950b0e
GabrielValle91 <gvalle91@gmail.com>
2018-08-30 17:15:48 -0500

    removed extra React files and created Found Pet Container

---
+++ client/src/containers/FoundPet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class FoundPet extends Component {
+
+    render(){
+        return (
+            <div>
+                Found Pet Container
+            </div>
+        )
+    }
+}
+
+export default FoundPet;
