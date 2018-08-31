c42fb140b894914a3f831b618f9452e75f418245
GabrielValle91 <gvalle91@gmail.com>
2018-08-30 20:08:33 -0500

    Refactored Found Pet Card

--- client/src/components/foundPets/FoundPet.js
+++ client/src/components/foundPets/FoundPet.js
@@ -1,17 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './FoundPets.css'
 
-class FoundPet extends Component {
-  render(){
-    const { foundPet } = this.props;
-
-    return (
-      <div className="FoundPetCard">
-        <p>{foundPet.gender} - {foundPet.animal_type}</p>
-        <p>Location: {foundPet.city}, {foundPet.state}</p>
-      </div>
-    )
-  }
-}
+const FoundPet = ({ foundPet }) => (
+  <div className="FoundPetCard">
+    <p>{foundPet.gender} - {foundPet.animal_type}</p>
+    <p>Location: {foundPet.city}, {foundPet.state}</p>
+  </div>
+)
 
 export default FoundPet;
