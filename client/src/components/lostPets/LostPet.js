31dd86ab2601098df7c0fdeb217b9eca27ded238
GabrielValle91 <gvalle91@gmail.com>
2018-09-01 18:51:37 -0500

    added lost pet card component

--- client/src/components/lostPets/LostPet.js
+++ client/src/components/lostPets/LostPet.js
@@ -3,7 +3,8 @@ import './LostPets.css';
 
 const LostPet = ({lostPet}) => {
   <div className="LostPetCard">
-    
+    <p>{lostPet.gender} - {lostPet.animal_type}</p>
+    <p>Location: {lostPet.city}, {lostPet.state}</p>
   </div>
 }
 
