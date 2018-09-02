49e9292db9817d570bb04ed42612bc5c66916c3b
GabrielValle91 <gvalle91@gmail.com>
2018-09-01 19:03:36 -0500

    added lost pet actions

--- client/src/actions/LostPets.js
+++ client/src/actions/LostPets.js
@@ -1,3 +1,5 @@
+import { resetLostPetForm } from './LostPetForm'
+
 const setLostPets = lostPets => {
   return {
     type: 'GET_LOST_PETS',
@@ -5,11 +7,37 @@ const setLostPets = lostPets => {
   }
 }
 
+const addLostPet = lostPet => {
+  return {
+    type: 'CREATE_LOST_PET',
+    lostPet
+  }
+}
 
 export const getLostPets = () => {
   return dispatch => {
     return fetch('http://localhost:3001/api/lost_pets')
     .then(response => response.json())
     .then(lostPets => dispatch(setLostPets(lostPets)))
+  }
+}
+
+export const createLostPet = lostPet => {
+  console.log(lostPet)
+  return dispatch => {
+    return fetch('http://localhost:3001/api/lost_pets', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({lost_pet: lostPet})
+    })
+      .then(response => response.json())
+      .then(lostPet => {
+        console.log(lostPet)
+        dispatch(addLostPet(lostPet))
+        dispatch(resetLostPetForm())
+      })
+      .catch(error => console.log(error))
   }
 }
