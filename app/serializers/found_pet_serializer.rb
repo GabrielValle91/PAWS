6db45579a02794767bd81c6b5274f86a9f32266b
GabrielValle91 <gvalle91@gmail.com>
2018-09-02 11:31:40 -0500

    added image url to found and lost pet serializers

--- app/serializers/found_pet_serializer.rb
+++ app/serializers/found_pet_serializer.rb
@@ -1,4 +1,4 @@
 class FoundPetSerializer < ActiveModel::Serializer
-    attributes :id, :animal_type, :city, :state, :area, :gender, :pet_image
+    attributes :id, :animal_type, :city, :state, :area, :gender, :pet_image, :image_url
     has_many :found_pet_comments
 end
