bc21c13c6eee03e6f5c13464db8598b7c0e460ac
GabrielValle91 <gvalle91@gmail.com>
2018-09-01 19:01:28 -0500

    added case for creating lost pet

--- client/src/reducers/LostPetReducer.js
+++ client/src/reducers/LostPetReducer.js
@@ -2,6 +2,8 @@ const LostPetReducer = (state = [], action) => {
   switch(action.type){
     case 'GET_LOST_PETS':
       return action.lostPets;
+    case 'CREATE_LOST_PET':
+      return state.concat(action.lostPet);
     default:
       return state;
   }
