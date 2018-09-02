01b8fa323b75fea64ef634bb9790eb197f83c1e5
GabrielValle91 <gvalle91@gmail.com>
2018-09-02 12:03:00 -0500

    Converted Found Pet Card Component into a class

--- client/src/components/foundPets/FoundPet.js
+++ client/src/components/foundPets/FoundPet.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './FoundPets.css'
 
-const FoundPet = ({ foundPet }) => (
-  <div className="FoundPetCard">
-    <p className="FoundPetCardContent">{foundPet.gender} - {foundPet.animal_type}</p>
-    <img src={foundPet.image_url} alt={"pet pic"} className="FoundPetImage"/>
-    <p className="FoundPetCardContent">Location: {foundPet.city}, {foundPet.state}</p>
-  </div>
-)
+// const FoundPet = ({ foundPet }) => (
+//   <div className="FoundPetCard">
+//     <p className="FoundPetCardContent">{foundPet.gender} - {foundPet.animal_type}</p>
+//     <img src={foundPet.image_url} alt={"pet pic"} className="FoundPetImage"/>
+//     <p className="FoundPetCardContent">Location: {foundPet.city}, {foundPet.state}</p>
+//   </div>
+// )
+class FoundPet extends Component {
+  render (){
+    const {foundPet} = this.props;
+    return(
+      <div className="FoundPetCard">
+        <p className="FoundPetCardContent">{foundPet.gender} - {foundPet.animal_type}</p>
+        <img src={foundPet.image_url} alt={"pet pic"} className="FoundPetImage"/>
+        <p className="FoundPetCardContent">Location: {foundPet.city}, {foundPet.state}</p>
+      </div>
+    )
+  }
+}
 
 export default FoundPet;
