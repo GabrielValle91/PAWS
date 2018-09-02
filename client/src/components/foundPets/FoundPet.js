71d08e94e4f2bdb10598696571ff342ca2d3b1cb
GabrielValle91 <gvalle91@gmail.com>
2018-09-02 15:19:15 -0500

    added component for Found Pet comments

--- client/src/components/foundPets/FoundPet.js
+++ client/src/components/foundPets/FoundPet.js
@@ -17,13 +17,14 @@ class FoundPet extends Component {
 
   render (){
     const {foundPet} = this.props;
+    
     return(
       <div className={this.state.isActive ? "FoundPetDetailCard" : "FoundPetCard"} onClick={this.handleClick}>
         <p className={this.state.isActive ? "FoundPetDetailContent" : "FoundPetCardContent"}>{foundPet.gender} - {foundPet.animal_type}</p>
         <img src={foundPet.image_url} alt={"pet pic"} className={this.state.isActive ? "FoundPetDetailImage" : "FoundPetImage"}/>
         <p className={this.state.isActive ? "FoundPetDetailContent" : "FoundPetCardContent"}>Location: {foundPet.city}, {foundPet.state}</p>
         {this.state.isActive && <p className="FoundPetDetailContent">{foundPet.area}</p>}
-        {this.state.isActive && <FoundPetComments />}
+        {this.state.isActive && <FoundPetComments comments={foundPet.found_pet_comments}/>}
       </div>
     )
   }
--- client/src/components/foundPets/FoundPetComments.js
+++ client/src/components/foundPets/FoundPetComments.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const FoundPetComments = ({ foundPet }) => (
-  <div>
-    <p>In comments component</p>
-  </div>
-)
+class FoundPetComments extends Component {
+  render(){
+    const {comments} = this.props;
+    const commentList = comments.map(comment => {
+      return(
+        <p>{comment.comment}</p>
+      )
+    })
+    return(
+      <div>
+        <p>In comments component</p>
+        {commentList}
+      </div>
+    )
+  }
+}
 
 export default FoundPetComments;
