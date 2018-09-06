3edf74c5d7f09b534e5c443812327837eaa54cd5
GabrielValle91 <gvalle91@gmail.com>
2018-09-05 21:44:49 -0500

    added fallback path to application controller and route

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   end
   post '/login', to: 'sessions#create'
   post '/signup', to: 'sessions#signup'
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
